0a8bd909efc06d23f49c09cb3516cad251371ab6
zbyti <zbyti@yahoo.com>
2020-12-02 01:01:33 +0100

    refactor

--- 09-PlayerGraphics/src/main.pas
+++ 09-PlayerGraphics/src/main.pas
@@ -27,13 +27,13 @@ procedure init;
 begin
   systemOff; DMACTL := 0;
 
-  PMBASE := hi(PM_ADDRESS);
+  PMBASE := hi(PM_ADR);
   hposp0 := 44; hposp1 := 52; HPOSP01 := shipHpos;
   COLPM01 := $0f0f; SIZEP01 := 0; PRIOR := 0; GRACTL := %00000011;
 
-  FillByte(pointer(PM_ADDRESS + MISSILES_OFFSET), $800 - MISSILES_OFFSET, 0);
-  Move(pointer(PLAYER_SHIP_ADDRESS), pointer(PM_ADDRESS + PLAYER0_OFFSET), PLAYER_SHIP_SEG);
-  Move(pointer(PLAYER_SHIP_ADDRESS + PLAYER_SHIP_SEG), pointer(PM_ADDRESS + PLAYER1_OFFSET), PLAYER_SHIP_SEG);
+  FillByte(pointer(PM_ADR + MISSILES_OFFSET), $800 - MISSILES_OFFSET, 0);
+  Move(pointer(PLAYER_SHIP_ADDRESS), pointer(PM_ADR + PLAYER0_OFFSET), PLAYER_SHIP_SEG);
+  Move(pointer(PLAYER_SHIP_ADDRESS + PLAYER_SHIP_SEG), pointer(PM_ADR + PLAYER1_OFFSET), PLAYER_SHIP_SEG);
 
   pause; DMACTL := %00111110; setVbi(@vbi);
 end;
