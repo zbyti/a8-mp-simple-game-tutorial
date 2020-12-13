d2ac7edb242eade781be938d65f0c5d813e9c3cd
zbyti <zbyti@yahoo.com>
2020-12-13 16:54:35 +0100

    only game

---
+++ game/lib/joy.pas
@@ -0,0 +1,52 @@
+unit joy;
+
+//---------------------- INTERFACE ---------------------------------------------
+
+interface
+
+var
+  joyDirection: byte absolute 3;
+
+procedure moveShip;
+
+//---------------------- IMPLEMENTATION ----------------------------------------
+
+implementation
+
+uses globals, sprites;
+
+procedure moveShip;
+begin
+  bMask1 := %1100;
+  for b1i := 1 downto 0 do begin
+    case (joyDirection and bMask1) of
+      JOY_RIGHT: begin
+        if bHposp1 < SHIP_RIGHT_LIMIT then begin
+          Inc(wShipX,SHIP_X_STEP); HPOSP01 := wShipX;
+        end;
+      end;
+      JOY_LEFT: begin
+        if bHposp0 > SHIP_LEFT_LIMIT then begin
+          Dec(wShipX,SHIP_X_STEP); HPOSP01 := wShipX;
+        end;
+      end;
+      JOY_UP: begin
+        if bShipY > SHIP_TOP_LIMIT then begin
+          Dec(bShipY,SHIP_Y_STEP); copyShip;
+        end;
+      end;
+      JOY_DOWN: begin
+        if bShipY < SHIP_BOTTOM_LIMIT then begin
+          Inc(bShipY,SHIP_Y_STEP); copyShip;
+        end;
+      end;
+    end;
+    bMask1 := %0011;
+  end;
+end;
+
+//---------------------- INITIALIZATION ----------------------------------------
+
+initialization
+
+end.
---
+++ game/lib/sprites.pas
@@ -0,0 +1,47 @@
+unit sprites;
+
+//---------------------- INTERFACE ---------------------------------------------
+
+interface
+
+var
+  bHposp0    : byte absolute 0;
+  bHposp1    : byte absolute 1;
+  bShipY     : byte absolute 2;
+  wShipX     : word absolute 0;
+
+procedure init;
+procedure copyShip;
+
+//---------------------- IMPLEMENTATION ----------------------------------------
+
+implementation
+
+uses globals;
+
+procedure copyShip;
+begin
+  FillByte(pointer(P0_ADR + bShipY - SHIP_Y_STEP), $10, 0);
+  FillByte(pointer(P1_ADR + bShipY - SHIP_Y_STEP), $10, 0);
+  Move(pointer(GFX_SHIP_ADR), pointer(P0_ADR + bShipY), GFX_SHIP_SEG);
+  Move(pointer(GFX_SHIP_ADR + GFX_SHIP_SEG), pointer(P1_ADR + bShipY), GFX_SHIP_SEG);
+end;
+
+procedure init;
+begin
+  FillByte(pointer(M0_ADR), $500, 0);
+  FillByte(pointer(M0_ADR + 32), $90, $ff);
+
+  PMBASE := hi(PM_ADR);
+  COLPM3 := $a; COLPM01 := SHIP_COL;
+  bShipY := 80; bHposp0 := SHIP_LEFT_LIMIT; bHposp1 := SHIP_LEFT_LIMIT + 8; HPOSP01 := wShipX;
+  SIZEP01 := 0; SIZEM := 0; PRIOR := %0000; GRACTL := %011;
+
+  copyShip;
+end;
+
+//---------------------- INITIALIZATION ----------------------------------------
+
+initialization
+
+end.
